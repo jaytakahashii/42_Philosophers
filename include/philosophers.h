e6176572dafe61da07710f89a8fc1c48b11dcef6
Jay Takahashi (42ubuntu) <qsakura14555@gmail.com>
2024-08-18 14:02:19 +0900

    add Makefile

--- include/philosophers.h
+++ include/philosophers.h
@@ -3,10 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: jtakahas <jtakahas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 01:45:43 by jay               #+#    #+#             */
-/*   Updated: 2024/08/05 01:45:45 by jay              ###   ########.fr       */
+/*   Updated: 2024/08/18 12:25:58 by jtakahas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+#endif

0bb699b6d325c2bf1972d547d13dd38e704aeb52
Jay Takahashi (42ubuntu) <qsakura14555@gmail.com>
2024-08-24 16:11:00 +0900

    使用可能関数のみで再現(ChatGPT)

--- experiment/pthred.c
+++ experiment/pthred.c
@@ -1,7 +1,9 @@
-#include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
 
 #define N 5
 #define THINKING 2
@@ -13,113 +15,85 @@
 int state[N];
 int phil[N] = { 0, 1, 2, 3, 4 };
 
-sem_t mutex;
-sem_t S[N];
+pthread_mutex_t mutex;
+pthread_mutex_t forks[N];
 
-void test(int phnum)
-{
-    if (state[phnum] == HUNGRY
-        && state[LEFT] != EATING
-        && state[RIGHT] != EATING) {
-        // state that eating
+void test(int phnum) {
+    if (state[phnum] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING) {
         state[phnum] = EATING;
+        
+        usleep(200000);  // Sleep for 200ms (equivalent to sleep(2) with more precision)
 
-        sleep(2);
-
-        printf("Philosopher %d takes fork %d and %d\n",
-                      phnum + 1, LEFT + 1, phnum + 1);
-
+        printf("Philosopher %d takes fork %d and %d\n", phnum + 1, LEFT + 1, phnum + 1);
         printf("Philosopher %d is Eating\n", phnum + 1);
-
-        // sem_post(&S[phnum]) has no effect
-        // during takefork
-        // used to wake up hungry philosophers
-        // during putfork
-        sem_post(&S[phnum]);
     }
 }
 
-// take up chopsticks
-void take_fork(int phnum)
-{
+void take_fork(int phnum) {
+    pthread_mutex_lock(&mutex);
 
-    sem_wait(&mutex);
-
-    // state that hungry
     state[phnum] = HUNGRY;
-
     printf("Philosopher %d is Hungry\n", phnum + 1);
 
-    // eat if neighbours are not eating
     test(phnum);
 
-    sem_post(&mutex);
+    pthread_mutex_unlock(&mutex);
 
-    // if unable to eat wait to be signalled
-    sem_wait(&S[phnum]);
-
-    sleep(1);
+    // If unable to eat, wait until the forks are available
+    pthread_mutex_lock(&forks[phnum]);
 }
 
-// put down chopsticks
-void put_fork(int phnum)
-{
+void put_fork(int phnum) {
+    pthread_mutex_lock(&mutex);
 
-    sem_wait(&mutex);
-
-    // state that thinking
     state[phnum] = THINKING;
-
-    printf("Philosopher %d putting fork %d and %d down\n",
-           phnum + 1, LEFT + 1, phnum + 1);
+    printf("Philosopher %d putting fork %d and %d down\n", phnum + 1, LEFT + 1, phnum + 1);
     printf("Philosopher %d is thinking\n", phnum + 1);
 
     test(LEFT);
     test(RIGHT);
 
-    sem_post(&mutex);
+    pthread_mutex_unlock(&forks[phnum]);
+    pthread_mutex_unlock(&mutex);
 }
 
-void* philosopher(void* num)
-{
+void* philosopher(void* num) {
+    int* i = num;
 
     while (1) {
-
-        int* i = num;
-
-        sleep(1);
+        usleep(100000);  // Sleep for 100ms (simulating thinking)
 
         take_fork(*i);
-
-        sleep(0);
-
+        usleep(100000);  // Sleep for 100ms (simulating eating)
         put_fork(*i);
     }
 }
 
-int main()
-{
-
+int main() {
     int i;
     pthread_t thread_id[N];
 
-    // initialize the semaphores
-    sem_init(&mutex, 0, 1);
-
-    for (i = 0; i < N; i++)
-
-        sem_init(&S[i], 0, 0);
+    // Initialize mutexes
+    pthread_mutex_init(&mutex, NULL);
+    memset(state, THINKING, sizeof(state));
+    for (i = 0; i < N; i++) {
+        pthread_mutex_init(&forks[i], NULL);
+    }
 
     for (i = 0; i < N; i++) {
-
-        // create philosopher processes
-        pthread_create(&thread_id[i], NULL,
-                       philosopher, &phil[i]);
-
+        pthread_create(&thread_id[i], NULL, philosopher, &phil[i]);
         printf("Philosopher %d is thinking\n", i + 1);
     }
 
-    for (i = 0; i < N; i++)
-
+    for (i = 0; i < N; i++) {
         pthread_join(thread_id[i], NULL);
+    }
+
+    // Destroy mutexes
+    pthread_mutex_destroy(&mutex);
+    for (i = 0; i < N; i++) {
+        pthread_mutex_destroy(&forks[i]);
+    }
+
+    return 0;
 }
